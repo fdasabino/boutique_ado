91762e04cdff9562c9ca89bb2e153d7779704de4
Francisco Sabino <fdasabino@gmail.com>
2021-11-03 11:10:38 +0100

    update css and templates

--- boutique_ado/urls.py
+++ boutique_ado/urls.py
@@ -6,6 +6,7 @@ from boutique_ado.settings import MEDIA_URL
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('products/', include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
