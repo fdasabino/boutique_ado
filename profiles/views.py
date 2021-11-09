9f340a8f6136bcf61f4bf8776ed412639f74702d
Francisco Sabino <Francisco Sabino>
2021-11-09 20:26:03 +0100

    created user profile app

--- profiles/views.py
+++ profiles/views.py
@@ -40,7 +40,7 @@ def order_history(request, order_number):
         ),
     )
 
-    template = "checkout/checkout_success.html"
+    template = "checkout_success.html"
     context = {
         "order": order,
         "from_profile": True,
